b13fe9de390c3ade5dd80f127b30f5bae5e68ff8
Hariharan <109100792+Eng-Hari@users.noreply.github.com>
2025-12-10 11:16:39 +0530

    Implement array equality check method
    Added a check method to compare two integer arrays for equality based on element counts.

--- Array_logic_1/Cheach_equals.java
+++ Array_logic_1/Cheach_equals.java
@@ -20,4 +20,32 @@ public class Cheach_equals {
         // }
         System.out.println(Arrays.equals(a, b));
     }
+
+private static boolean check(int[] a, int[] b) {
+        if (a.length != b.length) {
+            return false;
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : a) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : b) {
+            if (!map.containsKey(num)) {
+                return false;
+            }
+
+            map.put(num, map.get(num) - 1);
+
+            if (map.get(num) == 0) {
+                map.remove(num);
+            }
+        }
+        return map.isEmpty();
+
+    }
+
 }
+
